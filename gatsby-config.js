3e16109c24bfd4bf38a8df9c09af908f173dd343
Lewis Llobera <billobera@gmail.com>
2019-11-29 10:20:40 +0100

    Remove unneeded dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     twitter: `@lewislbr`,
     email: `billobera@gmail.com`,
     siteUrl: `https://www.lewis-gatsby-starter-i18n.firebaseapp.com`,
-    gitUrl: `https://github.com/lewislbr/lewis-gatsby-starter-i18n`
+    gitUrl: `https://github.com/lewislbr/lewis-gatsby-starter-i18n`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,27 +19,8 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Lewis Gatsby Starter i18n`,
-        short_name: `LGSi18n`,
-        start_url: `/`,
-        background_color: `#fcfcfc`,
-        theme_color: `#fcfcfc`,
-        display: `standalone`,
-      },
-    },
     `gatsby-plugin-offline`,
     `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-134643417-6',
-        head: true,
-        anonymize: true,
-      },
-    },
     {
       resolve: `gatsby-plugin-intl`,
       options: {
@@ -52,4 +33,4 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
   ],
-}
+};
