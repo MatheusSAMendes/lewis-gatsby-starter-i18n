819c8720d4a24c02381b64ec7ee423d617fd9928
Lewis Llobera <billobera@gmail.com>
2019-11-29 10:54:22 +0100

    Add CSS reset

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -3,88 +3,124 @@ import { createGlobalStyle } from 'styled-components';
 import * as font from '../assets/fonts';
 
 export const GlobalStyles = createGlobalStyle`
-* {
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-}
-
-:root {
-  --sides-padding-desktop: 3%;
-  --sides-padding-mobile: 5%;
-  --light-color: hsl(0, 0%, 99%);
-  --light-color-translucent: hsla(0, 0%, 99%, 0.92);
-  --dark-color: hsl(0, 0%, 15%);
-  --dark-color-light: hsla(0, 0%, 15%, 0.9);
-  --dark-color-lighter: hsla(00, 0%, 15%, 0.7);
-  --accent-color: hsl(339, 100%, 55%);
-}
-
-@font-face {
-  font-family: "IBMPlexMono";
-  src: url('${font.IBMPlexMono400}') format("woff2");
-  font-weight: 400;
-  font-style: normal;
-  font-display: block;
-}
-
-@font-face {
-  font-family: "IBMPlexMono";
-  src: url('${font.IBMPlexMono700}') format("woff2");
-  font-weight: 700;
-  font-style: normal;
-  font-display: block;
-}
-
-body {
-  height: 100%;
-  width: 100%;
-  margin: 0;
-  margin-left: auto;
-  margin-right: auto;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  padding-top: 20vh;
-  line-height: 1;
-  background-color: var(--light-color);
-  font-family: 'IBMPlexMono', sans-serif;
-  font-size: 19px;
-  font-weight: 400;
-  color: var(--dark-color);
-
-  @media (max-width: 849px) {
-    font-size: 17px;
+  :root {
+    --sides-padding-desktop: 3%;
+    --sides-padding-mobile: 5%;
+    --light-color: hsl(0, 0%, 99%);
+    --light-color-translucent: hsla(0, 0%, 99%, 0.92);
+    --dark-color: hsl(0, 0%, 15%);
+    --dark-color-light: hsla(0, 0%, 15%, 0.9);
+    --dark-color-lighter: hsla(00, 0%, 15%, 0.7);
+    --accent-color: hsl(339, 100%, 55%);
   }
-}
 
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  margin: 0;
-  -webkit-margin-before: 0;
-  margin-block-start: 0;
-  -webkit-margin-after: 0;
-  margin-block-end: 0;
-  -webkit-margin-start: 0;
-  margin-inline-start: 0;
-  -webkit-margin-end: 0;
-  margin-inline-end: 0;
-  display: inline-block;
-}
+  @font-face {
+    font-family: "IBMPlexMono";
+    src: url('${font.IBMPlexMono400}') format("woff2");
+    font-weight: 400;
+    font-style: normal;
+    font-display: block;
+  }
 
-a {
-  text-decoration: none;
-  -webkit-tap-highlight-color: hsla(0, 0%, 0%, 0);
-  -webkit-tap-highlight-color: transparent;
-  color: inherit;
-}
+  @font-face {
+    font-family: "IBMPlexMono";
+    src: url('${font.IBMPlexMono700}') format("woff2");
+    font-weight: 700;
+    font-style: normal;
+    font-display: block;
+  }
 
-img {
-  max-width: 100%;
-  max-height: 100%;
-  height: 100%;
-  width: 100%;
-}
+  body {
+    padding-top: 20vh;
+    background-color: var(--light-color);
+    font-family: 'IBMPlexMono', sans-serif;
+    font-size: 19px;
+    font-weight: 400;
+    color: var(--dark-color);
+
+    @media (max-width: 849px) {
+      font-size: 17px;
+    }
+  }
+
+  /* CSS Reset */
+
+  /* Box sizing rules */
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  /* Remove default padding */
+  ol[class],
+  ul[class] {
+    padding: 0;
+  }
+
+  /* Remove default margin */
+  blockquote,
+  body,
+  dd,
+  dl,
+  figcaption,
+  figure,
+  h1,
+  h2,
+  h3,
+  h4,
+  li,
+  ol[class],
+  p,
+  ul[class] {
+    margin: 0;
+  }
+
+  /* Set core body defaults */
+  body {
+    line-height: 1.5;
+    min-height: 100vh;
+    scroll-behavior: smooth;
+    text-rendering: optimizeSpeed;
+  }
+
+  /* Remove list styles on ul, ol elements with a class attribute */
+  ul[class],
+  ol[class] {
+    list-style: none;
+  }
+
+  /* A elements that don't have a class get default styles */
+  a:not([class]) {
+    text-decoration-skip-ink: auto;
+  }
+
+  /* Make images easier to work with */
+  img {
+    display: block;
+    max-width: 100%;
+  }
+
+  /* Natural flow and rhythm in articles by default */
+  article > * + * {
+    margin-top: 1em;
+  }
+
+  /* Inherit fonts for inputs and buttons */
+  button,
+  input,
+  select,
+  textarea {
+    font: inherit;
+  }
+
+  /* Remove all animations and transitions for people that prefer not to see them */
+  @media (prefers-reduced-motion: reduce) {
+    * {
+      animation-duration: 0.01ms !important;
+      animation-iteration-count: 1 !important;
+      scroll-behavior: auto !important;
+      transition-duration: 0.01ms !important;
+    }
+  }
 `;
